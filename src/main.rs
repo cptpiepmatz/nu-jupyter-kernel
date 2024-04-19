6fde7b03efb1f42b82b6050d23d8b5efdfc50c12
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2024-04-19 11:15:08 +0200

    moved some nu-related code into own module

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use register_kernel::{register_kernel, RegisterLocation};
 
 mod connection_file;
 mod register_kernel;
+mod execute_nu;
 
 static_toml::static_toml! {
     const CARGO_TOML = include_toml!("Cargo.toml");
@@ -39,39 +40,6 @@ enum Command {
     Start {
         connection_file_path: PathBuf,
     },
-}
-
-static INPUT: &str = "{a: 3} | to json";
-
-fn get_engine_state() -> EngineState {
-    // TODO: compare with nu_cli::get_engine_state for other contexts
-    let engine_state = nu_cmd_lang::create_default_context();
-    let engine_state = nu_command::add_shell_command_context(engine_state);
-    engine_state
-}
-
-fn execute_some_input() {
-    let input = INPUT.as_bytes();
-    let mut engine_state = get_engine_state();
-    let mut working_set = StateWorkingSet::new(&engine_state);
-    let block = nu_parser::parse(&mut working_set, None, input, false);
-
-    if let Some(error) = working_set.parse_errors.first() {
-        println!("got some error: {error}");
-        return;
-    }
-
-    engine_state
-        .merge_delta(working_set.delta)
-        .expect("could not merge delta");
-    let mut stack = Stack::new();
-    let res = nu_engine::eval_block::<WithoutDebug>(
-        &engine_state,
-        &mut stack,
-        &block,
-        PipelineData::empty(),
-    );
-    dbg!(res);
 }
 
 #[tokio::main]
